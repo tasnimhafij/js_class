4eba39f01a2bb9cf05bf5953029ee5a5ff0d6b1a
tasnimhafij <tasnimhafij@gmail.com>
2018-07-10 04:57:38 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+
+var name = "hello world";
+
+console.log(name);
+
+var number =10;
+
+var name = "tasmim";
+
+console.log(typeof(number));
+console.log(typeof(name));
+
+var js;
+
+console.log(js);
+
+//********
+var num1= "4";
+var num2= 4;
+
+console.log(num1+num2);
+console.log(num2+num1);
+
+
+var a = 5,
+c=6,
+d=90;
+
+var a;
+
+console.log(a);
+console.log(d);
+
+
+var object = {firstname:"tasnim", age:23};
+
+console.log(object);
+
+
+var array = ['tasnim','5',6];
+
+console.log(array);
+
+var game= function array(){
+
+}
+
+console.log(typeof(game));
+
+var mode = myfunction(33);
+
+function myfunction(a) {
+	return a * 10;
+}
+
+console.log(mode);
+
+
+
+
+
